f655c7d63c6ba3fc81e70b176fa743ae2c3a313e
Mok235g <131176167+Mok235g@users.noreply.github.com>
2023-08-14 21:24:33 +0700

    Update t.py

--- t.py
+++ t.py
@@ -14,6 +14,7 @@ def api():
         }
 
         headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows 98; Win 9x 4.90) AppleWebKit/5332 (KHTML, like Gecko) Chrome/36.0.870.0 Mobile Safari/5332',
             'Host': 'www.arcshop.in.th',
         'Cookie': 'arcshop=7bkpbngo1p0qk5mi7nigs37nek8rntck; _gcl_au=1.1.1299685118.1692013478; _gid=GA1.3.1152518994.1692013479; cf_clearance=eIVAMaKxIm.j5Z8aE6fVfzjDM1Ry5rELAJrvo_yakk4-1692013482-0-1-8d136230.8eee76af.da123dd0-0.2.1692013482; twk_idm_key=it5BVBGwbQZuDXGGd0-Q1; arcshopaccount=kqODrMcgCDSKGtXPJlsxaooXiGaeyqfmWMhAEQzdHbAcdrjjtuIvwHvUNxpiVTTs; _ga_WPNMZDR72H=GS1.1.1692013478.1.1.1692013498.0.0.0; _ga=GA1.3.642151656.1692013479; _ga_30PLGWBTMP=GS1.3.1692013482.1.1.1692013499.43.0.0; TawkConnectionTime=0',
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
